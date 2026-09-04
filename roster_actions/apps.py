a2f882c776928fbb2b48de97bf21397297ed1de3
pastorhudson <3606628+pastorhudson@users.noreply.github.com>
2026-09-03 21:18:00 -0400

    Rename `transactions` app to `roster_actions` for improved clarity and alignment with functionality.

--- roster_actions/apps.py
+++ roster_actions/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TransactionsConfig(AppConfig):
+class RosterActionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transactions'
+    name = 'roster_actions'
